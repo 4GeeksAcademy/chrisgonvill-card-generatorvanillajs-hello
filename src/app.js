7bd0db86aff297353b2abe21eabcf8b50deb47f2
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2023-10-05 00:10:23 +0000

    cards

--- src/app.js
+++ src/app.js
@@ -6,6 +6,42 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  const denominations = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const cardElement = document.querySelector(".card");
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomDenomination =
+    denominations[Math.floor(Math.random() * denominations.length)];
+
+  cardElement.querySelector(".top-suit").innerHTML = randomSuit;
+  cardElement.querySelector(".denomination").innerHTML = randomDenomination;
+  cardElement.querySelector(".bottom-suit").innerHTML = randomSuit;
+  cardElement
+    .querySelector(".top-suit")
+    .classList.add(getSuitClass(randomSuit));
+  cardElement
+    .querySelector(".bottom-suit")
+    .classList.add(getSuitClass(randomSuit));
 };
+
+function getSuitClass(suit) {
+  if (suit === "♦" || suit === "♥") {
+    return "hearts";
+  } else if (suit === "♠" || suit === "♣") {
+    return "spades";
+  }
+}
